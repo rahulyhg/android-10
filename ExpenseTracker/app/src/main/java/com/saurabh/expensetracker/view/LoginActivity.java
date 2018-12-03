fb31a6bbeee6f8dcb10841fe9a6de2c55f31c6a0
saurabh1489 <saurabh1489@gmail.com>
2018-12-04 00:34:30 +0530

    Adding password & email fields

--- ExpenseTracker/app/src/main/java/com/saurabh/expensetracker/view/LoginActivity.java
+++ ExpenseTracker/app/src/main/java/com/saurabh/expensetracker/view/LoginActivity.java
@@ -2,9 +2,17 @@ package com.saurabh.expensetracker.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.saurabh.expensetracker.DaggerFactory;
 import com.saurabh.expensetracker.R;
 import com.saurabh.expensetracker.databinding.ActivityLoginBinding;
@@ -12,6 +20,7 @@ import com.saurabh.expensetracker.db.entities.UserCredential;
 import com.saurabh.expensetracker.enums.LoginEnum;
 import com.saurabh.expensetracker.viewmodel.LoginViewModel;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
@@ -22,7 +31,10 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class LoginActivity extends AppCompatActivity {
+    private final static String TAG = LoginActivity.class.getSimpleName();
     private LoginViewModel viewModel;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final static int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +44,12 @@ public class LoginActivity extends AppCompatActivity {
         viewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
         binding.setUserCredential(credential);
         viewModel.setUserCredential(credential);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(getApplicationContext(), gso);
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setOnClickListener(v -> processGoogleSignin());
     }
 
     public void openSignupActivity(View view) {
@@ -42,6 +60,31 @@ public class LoginActivity extends AppCompatActivity {
     public void openContentScreen() {
         Intent intent = new Intent(this, ContentActivity.class);
         startActivity(intent);
+    }
+
+    public void processGoogleSignin() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (resultCode) {
+            case RC_SIGN_IN:
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                handleSignInResult(task);
+                break;
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.d(TAG, "Login Successful");
+        } catch (ApiException e) {
+            Log.d(TAG, "signInResult:failed code=" + e.getStatusCode());
+        }
     }
 
     public void processLogin(View view) {
--- ExpenseTracker/app/src/main/java/com/saurabh/expensetracker/view/MainActivity.java
+++ ExpenseTracker/app/src/main/java/com/saurabh/expensetracker/view/MainActivity.java
@@ -3,6 +3,8 @@ package com.saurabh.expensetracker.view;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.saurabh.expensetracker.R;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        openLoginScreen();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        if (account == null) {
+            openActivity(LoginActivity.class);
+        } else {
+            openActivity(ContentActivity.class);
+        }
     }
 
-    private void openLoginScreen() {
-        Intent loginIntent = new Intent(this,LoginActivity.class);
+    private void openActivity(Class className) {
+        Intent loginIntent = new Intent(this, className);
         startActivity(loginIntent);
         finish();
     }
